0c408587681cc450b9f1a6ee940fbb522ba07df7
JulienJ-dev <julienjedrzejewski@gmail.com>
2026-02-24 16:43:47 +0100

    minor improvments

--- main_scrapping_books_toscrape.py
+++ main_scrapping_books_toscrape.py
@@ -3,40 +3,7 @@ import csv
 from pathlib import Path
 from bs4 import BeautifulSoup
 
-def download_book_image(category_products_infos, category_name, base_dir): # Cette fonction prend en paramètres la liste des infos des livres de la catégorie et le nom de la catégorie
-    i = 0
-    FOLDER = base_dir / "Books_visuals"
-    CHILD_FOLDER = FOLDER / category_name
-    FOLDER.mkdir(parents=True, exist_ok = True)
-    CHILD_FOLDER.mkdir(parents=True, exist_ok = True)
-    print( category_name + " books visuals download in progress")
-    for element in category_products_infos:
-        i += 1
-        print("downloading " + str(i) + " on " + str(len(category_products_infos)) + " images")
-        title = element["title"]
-        for char in r'\/:*?"<>|':
-            title = title.replace(char, "-")
-        url_image = element["image url"]
-        filepath = CHILD_FOLDER / f"{title}.jpg"
-        response = requests.get(url_image)
-        if response.ok:
-            with filepath.open (mode="wb") as f:
-                f.write(response.content)
-    
-    return
-    
-def export_csv(csv_name, list_to_implement, base_dir):     # Cette fonction prend en paramètres le nom du csv à ouvrir/créer, et la liste des données à y implanter, elle crée un dossier dans lequel elle créera les csv contenant les données des articles de chaque catégorie
-    FOLDER = base_dir / "Books_toscrape_datas"
-    FOLDER.mkdir(parents=True, exist_ok=True) # Création d'un dossier pour contenir les csv et ainsi éviter la sauvegarde à la racine
-    filepath = FOLDER / csv_name
-    headers = list_to_implement[0].keys()     # La liste des headers est générée en récupérant toutes les clés du dictionnaire présent dans la liste via la variable headers
-    with filepath.open (mode="w", newline="", encoding = "utf-8-sig") as fichier:
-        writer = csv.DictWriter(fichier, fieldnames = headers, delimiter =";")     # Le fichier s'ouvre, les entêtes sont générés avec .writeheader() et les données sont implantées via writerows(liste)
-        writer.writeheader()
-        writer.writerows(list_to_implement)
-    return
-
-def check_category_pages_quantity(base_url, page_content): # Cette fonction récupère en paramètres l'url de la page et son contenu. Elle vérifie le nombre de pages pour chaque categories et renvoie la liste des urls de chaque page
+def check_category_pages_quantity(base_url, page_content): # Paramètres : l'url de la page et son contenu. Vérifie le nombre de pages pour chaque categorie et renvoie la liste des urls de chaque page
     pages_urls = []
     if page_content.find("div", class_ = "col-sm-8 col-md-9").find("li", class_ = "current"): # La fonction teste la présence de la balise li class = "current" contenant la chaine de caractères du bas de page "Page 1 of ..."
         page_quantity = page_content.find("div", class_ = "col-sm-8 col-md-9").find("li", class_ = "current").get_text(strip=True).replace("Page 1 of ", "") # Le texte est récupéré et supprimé, sauf le dernier chiffre, indiquant le nombre total de page dans la catégorie ciblée
@@ -46,8 +13,8 @@ def check_category_pages_quantity(base_url, page_content): # Cette fonction réc
     else :
         pages_urls.append(base_url)
         return pages_urls    # La fonction retourne un tableau contenant toutes les urls des pages de la categorie grâce à la boucle for
-
-def collect_urls_products(pages_urls) : # Cette fonction utilise en paramètre la liste des urls des pages à scrapper, elle va récupérer toutes les urls de chaque article de la catégorie et les retourner dans une liste
+    
+def collect_urls_products(pages_urls) : # Paramètre : la liste des urls des pages à scrapper. Récupére toutes les urls de chaque article de la catégorie et les retourne dans une liste
     urls_products = []
     for url in pages_urls :     #Une boucle for cherche et récupère dans chaque page le code html
         response = requests.get(url)
@@ -58,11 +25,15 @@ def collect_urls_products(pages_urls) : # Cette fonction utilise en paramètre l
             urls_products.append("https://books.toscrape.com/catalogue/" + a["href"].replace("../../../", ""))
     return urls_products
 
-def scrapping_infos_per_category(products_urls):     # Cette fonction prend en paramètres le tableau des urls de chaque article, elle récupère toutes les données ciblées de chaque article
+def scrapping_infos_per_category(products_urls, category_name):     # Paramètres : le tableau des urls de chaque article et le nom de la catégorie. Récupère toutes les données ciblées de chaque article
+    i = 0
     category_infos_list = []
     for url in products_urls:     # Une boucle for permet de passer sur chaque url présente dans la liste products_urls pour en récupérer le contenu et ajouter au dict product_infos, toutes les données ciblées
+        i+= 1
+        progress = round(i / len(products_urls) * 100)
         product_infos = {}
-        print(f"scrapping in progress on {url}")
+        print(f"Scrapping progress for the {category_name} category : {progress} %")
+        #print(f"scrapping in progress on {url}")
         response = requests.get(url)
         if response.ok:
             product_infos["url"] = url
@@ -112,19 +83,54 @@ def scrapping_infos_per_category(products_urls):     # Cette fonction prend en p
             category_infos_list.append(product_infos)    # Le dict est ensuite ajouté à la liste category_infos_list qui est retournée
     return category_infos_list
 
+def download_book_image(category_products_infos, category_name, base_dir): # Paramètres : la liste des infos des livres de la catégorie, le nom de la catégorie et le chemin du fichier racine. Télécharge les images de chaque article et les ajoute dans un dossier spécifique par catégorie.
+    i = 0 # compteur pour indication de l'avancée des téléchargements
+    FOLDER = base_dir / "Books_visuals"
+    CHILD_FOLDER = FOLDER / category_name
+    FOLDER.mkdir(parents=True, exist_ok = True)
+    CHILD_FOLDER.mkdir(parents=True, exist_ok = True)
+    print( category_name + " books visuals download in progress")
+    for element in category_products_infos:
+        i += 1
+        print("downloading " + str(i) + " on " + str(len(category_products_infos)) + " images")
+        title = element["title"]
+        for char in r'\/:*?"<>|':
+            title = title.replace(char, "-")
+        url_image = element["image url"]
+        filepath = CHILD_FOLDER / f"{title}.jpg"
+        response = requests.get(url_image)
+        if response.ok:
+            with filepath.open (mode="wb") as f:
+                f.write(response.content)
+    
+    return
+    
+def export_csv(csv_name, list_to_implement, base_dir):     # Cette fonction prend en paramètres le nom du csv à ouvrir/créer, et la liste des données à y implanter, elle crée un dossier dans lequel elle créera les csv contenant les données des articles de chaque catégorie
+    FOLDER = base_dir / "Books_toscrape_datas"
+    FOLDER.mkdir(parents=True, exist_ok=True) # Création d'un dossier pour contenir les csv et ainsi éviter la sauvegarde à la racine
+    filepath = FOLDER / csv_name
+    headers = list_to_implement[0].keys()     # La liste des headers est générée en récupérant toutes les clés du dictionnaire présent dans la liste via la variable headers
+    with filepath.open (mode="w", newline="", encoding = "utf-8-sig") as fichier:
+        writer = csv.DictWriter(fichier, fieldnames = headers, delimiter =";")     # Le fichier s'ouvre, les entêtes sont générés avec .writeheader() et les données sont implantées via writerows(liste)
+        writer.writeheader()
+        writer.writerows(list_to_implement)
+    return
+
 
 #main
 
 base_dir = Path(__file__).resolve().parent
 url = "https://books.toscrape.com"
-print("Scrapping on " + url + " launched")
 scrapping_progess = 0
 books_quantity = 0
 response = requests.get(url)
 if response.ok:
+    print("Scrapping on " + url + " launched")
     page_content = BeautifulSoup(response.content, "lxml")
     if page_content.find("form", method="get", class_="form-horizontal"):
         books_quantity = page_content.find("form", method="get", class_="form-horizontal").find_next("strong").get_text(strip=True)
+    else:
+        print("Error during searching the book quantity : form, method=get, class_=form-horizontal).find_next(strong)")
     if page_content.find("ul", class_="nav nav-list").find_next("ul").find("li"):
         for element in page_content.find("ul", class_="nav nav-list").find_next("ul").find_all("li"):
             pages_urls=[]
@@ -136,14 +142,14 @@ if response.ok:
                 category_page_content = BeautifulSoup(category_page_response.content, "lxml")
                 pages_urls.extend(check_category_pages_quantity(new_category_url,category_page_content))
                 products_urls = collect_urls_products(pages_urls)
-                books_infos_per_category = scrapping_infos_per_category(products_urls)
+                books_infos_per_category = scrapping_infos_per_category(products_urls, category_name)
                 scrapping_progess += len(books_infos_per_category)
-                print("\nScrapping progress : " + str(round(scrapping_progess / int(books_quantity) *100, 2)) + "%\n")
+                print("\nGeneral scrapping progress : " + str(round(scrapping_progess / int(books_quantity) *100, 2)) + "%\n")
                 export_csv(category_name +".csv", books_infos_per_category, base_dir)
                 print(category_name +".csv succesfully updated\n")
                 download_book_image(books_infos_per_category, category_name, base_dir)
                 print("\nImages for each book in "+ category_name + " category successfully downloaded\n")
     else:
-        print("Error during searching categories' urls")
+        print("Error during searching categories : ul, class_=nav nav-list).find_next(ul).find(li)")
         exit
     print("Scrapping ended, all CSV files and images updated")
